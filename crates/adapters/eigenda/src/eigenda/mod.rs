66693767a5ed968bb44f6358f1d58e13f5e4c421
Diogo Friggo <diogo@eiger.co>
2025-08-19 11:16:00 +0200

    feat: integrate cert verification (https://github.com/eigerco/sov-eigenda-adapter/pull/47)

--- crates/adapters/eigenda/src/eigenda/mod.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(feature = "native")]
-pub mod extraction;
-
-#[cfg(feature = "native")]
-pub mod proxy;
-
-pub mod types;
-pub mod validation;
--- crates/adapters/eigenda/src/lib.rs
+++
@@ -1,6 +0,0 @@
-mod eigenda;
-mod ethereum;
-#[cfg(feature = "native")]
-pub mod service;
-pub mod spec;
-pub mod verifier;
